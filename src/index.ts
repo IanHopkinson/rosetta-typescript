70bf0172ad9ac07b775a5f6c37057ac468b350da
Ian Hopkinson <ianhopkinson@googlemail.com>
2023-10-27 16:58:23 +0100

    Add promises version of word count

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,38 @@ async function main() {
     process.exit();
   }
 
-  const word_count = await word_statistics(file_path);
+  const word_count_promise = word_statistics_promise(file_path);
+  word_count_promise.then((word_count) => {
+    console.log(
+      `File "${file_path}" contains ${word_count} words using promises`
+    );
+  });
 
-  console.log(`File "${file_path}" contains ${word_count} words`);
+  const word_count = await word_statistics(file_path);
+  console.log(
+    `File "${file_path}" contains ${word_count} words using async/await`
+  );
+}
+
+export function word_statistics_promise(file_path: string) {
+  let word_count: number = 0;
+
+  const reader = rd.createInterface(fs.createReadStream(file_path));
+
+  return new Promise(function (resolve, reject) {
+    reader.on("line", (line: string) => {
+      const words = line.split(" ");
+      word_count += words.length;
+    });
+
+    reader.on("close", () => {
+      resolve(word_count);
+    });
+
+    reader.on("error", (error) => {
+      reject(error);
+    });
+  });
 }
 
 export async function word_statistics(file_path: string): Promise<number> {
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { word_statistics } from "../src/index";
+import { word_statistics, word_statistics_promise } from "../src/index";
 import path from "path";
 
 const file_path: string = path.join(
@@ -7,7 +7,14 @@ const file_path: string = path.join(
   "file_for_word_count_test.txt"
 );
 
-test("The word count is 6, as expected", async () => {
+test("The word count is 6, as expected (async/await)", async () => {
   const word_count = await word_statistics(file_path);
   expect(word_count).toBe(6);
+});
+
+test("The word count is 6, as expected (promises)", () => {
+  const word_count_promise = word_statistics_promise(file_path);
+  word_count_promise.then((word_count) => {
+    expect(word_count).toBe(6);
+  });
 });
